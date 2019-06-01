d88934f08113209a4e4ec266a4b0fce3245b6091
System Administrator <root@Diegos-MacBook-Pro.local>
2019-05-31 22:33:05 -0500

    added a way to crud tasks without token

--- projection-backend/api/utils/session.js
+++ projection-backend/api/utils/session.js
@@ -1,10 +1,14 @@
 const jwt = require('jsonwebtoken');
 
 const user = function(req) {
-    const token = req.headers.authorization;
-    const decoded = jwt.verify(token, process.env.JWT_KEY);
-    req.userData = decoded;
-    return decoded.id;
+    if (typeof req === 'number') {
+        return req;
+    } else {
+        const token = req.headers.authorization;
+        const decoded = jwt.verify(token, process.env.JWT_KEY);
+        req.userData = decoded;
+        return decoded.id;
+    }
 }
 
 module.exports = user;
